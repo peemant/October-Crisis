2cef846b03d784afe001dc10623da2d7087d34d0
peemant <space.time.traveller@gmail.com>
2015-12-29 04:54:24 -0500

    Initial commit

---
+++ October Crisis/Vehicules.swift
@@ -0,0 +1,13 @@
+//
+//  Vehicules.swift
+//  October Crisis
+//
+//  Created by Philippe Kahr on 2015-12-25.
+//  Copyright © 2015 Philippe Kahr. All rights reserved.
+//
+
+import UIKit
+
+class Vehicules: NSObject {
+
+}
